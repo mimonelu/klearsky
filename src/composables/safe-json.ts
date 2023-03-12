e08121ddd087a121166ea1ab8fcaf858b3f55624
mimonelu <mimonelu@gmail.com>
2023-03-12 17:58:22 +0900

    SafeJSON を追加

---
+++ src/composables/safe-json.ts
@@ -0,0 +1,19 @@
+export default {
+  stringify (json: any): null | string {
+    try {
+      return JSON.stringify(json)
+    } catch (error: any) {
+      console.error(error)
+      return null
+    }
+  },
+
+  parse (jsonString: string): null | any {
+    try {
+      return JSON.parse(jsonString)
+    } catch (error: any) {
+      console.error(error)
+      return null
+    }
+  }
+}
--- src/composables/storage.ts
+++ src/composables/storage.ts
@@ -3,33 +3,24 @@ import {
   encrypt,
   decrypt
 } from "@/composables/cipher"
+import SafeJSON from "@/composables/safe-json"
 
 export default {
-  save (key: string, json: undefined | null | any) {
+  save (key: string, json: null | any) {
     if (json == null) return
-    let jsonString = null
-    try {
-      jsonString = JSON.stringify(json)
-    } catch (error: any) {
-      console.error(error)
-      return
-    }
+    const jsonString: null | string = SafeJSON.stringify(json)
+    if (jsonString == null) return
     const cryptedKey = encryptAsHash(key)
     const cryptedJson = encrypt(jsonString)
     localStorage.setItem(cryptedKey, cryptedJson)
   },
 
-  load (key: string): undefined | null | any {
+  load (key: string): null | any {
     const cryptedKey = encryptAsHash(key)
     const cryptedJson: null | string = localStorage.getItem(cryptedKey)
     if (cryptedJson == null) return null
     const jsonString = decrypt(cryptedJson)
-    try {
-      return JSON.parse(jsonString)
-    } catch (error: any) {
-      console.error(error)
-      return null
-    }
+    return SafeJSON.parse(jsonString)
   },
 
   remove (key: string) {
