ddfe11b4a0a783e96051b449f6bdb1fe5bbec4a5
mimonelu <mimonelu@gmail.com>
2023-03-17 16:53:55 +0900

    pinia を削除

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import "@/scss/main.scss"
 import { createApp } from "vue"
-import { createPinia } from "pinia"
 import App from "@/App.vue"
 import messages from "@/consts/messages.json"
 import i18n from "@/plugins/i18n"
 import router from "@/router"
 
 const app = createApp(App)
-app.use(createPinia())
 app.use(router)
 app.use(i18n, messages)
 app.mount("#app")
