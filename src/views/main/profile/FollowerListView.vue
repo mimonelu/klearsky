613cd0c7f75d1afab06d7c9dfa0085791c908ffe
mimonelu <mimonelu@gmail.com>
2023-03-18 21:56:08 +0900

    UserList の作成

--- src/views/main/profile/FollowerListView.vue
+++ src/views/main/profile/FollowerListView.vue
@@ -1,43 +1,9 @@
 <script lang="ts" setup>
-import { inject } from "vue"
-import { useRouter } from "vue-router"
-
-const mainState = inject("state") as MainState
-
-const router = useRouter()
-
-async function openProfile (handle: string) {
-  await router.push({ name: "profile-post", query: { handle } })
-}
+import UserList from "@/components/UserList.vue"
 </script>
 
 <template>
-  <div class="user-list">
-    <div
-      v-for="user of mainState.currentUsers"
-      class="user"
-    >
-      <div
-        class="avatar"
-        @click.prevent="openProfile(user.handle)"
-      >
-        <img
-          :src="user.avatar ?? '/img/void-avatar.png'"
-          loading="lazy"
-        >
-      </div>
-      <div class="display-name">{{ user.displayName }}</div>
-      <div class="handle">{{ user.handle }}</div>
-      <div class="relations">
-        <div
-          v-if="user.viewer.following"
-          class="you-following"
-        >Following</div>
-        <div
-          v-if="user.viewer.followedBy"
-          class="you-followed"
-        >Followed</div>
-      </div>
-    </div>
+  <div class="follower-list-view">
+    <UserList />
   </div>
 </template>
