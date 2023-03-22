1e9d379da0a0f28178ad2e0551f326e5d5c0fca7
mimonelu <mimonelu@gmail.com>
2023-03-22 19:25:12 +0900

    型名のリファクタリングの変換ミスを修正

--- src/views/main/PostView.vue
+++ src/views/main/PostView.vue
@@ -7,6 +7,6 @@ const mainState = inject("state") as MainState
 
 <template>
   <div class="post-view">
-    <TTFeedThread :feeds="mainState.currentFeeds" />
+    <FeedThread :feeds="mainState.currentFeeds" />
   </div>
 </template>
