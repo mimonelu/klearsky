b7cb3b3fbcfd720a5517d436b94f47b56ca6225e
mimonelu <mimonelu@gmail.com>
2023-03-30 00:37:57 +0900

    通知まわりを微修正

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -353,9 +353,11 @@ async function fetchNotifications (limit: number, direction: "new" | "old") {
 
 async function updateNotification (forceUpdate: boolean) {
   const count = await state.atp.fetchNotificationCount() ?? 0
-  if (count > 0) state.notificationCount = count
-  if (count > 0 || forceUpdate)
-    await state.fetchNotifications(forceUpdate ? 50 : Math.min(50, count), "new")
+  if (count > state.notificationCount) {
+    state.notificationCount = count
+    if (forceUpdate)
+      await state.fetchNotifications(forceUpdate ? 50 : Math.min(50, count), "new")
+  }
 }
 
 let isSendPostDone = false
