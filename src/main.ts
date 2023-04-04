0edeb19f15834e0dcb2d58875189d1855be10b33
mimonelu <mimonelu@gmail.com>
2023-04-04 19:30:17 +0900

    Firefox における Intl.Segmenter 関連のエラーへの暫定対応

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import messages from "@/consts/messages.json"
 import i18n from "@/plugins/i18n"
 import router from "@/router"
 
+// Firefox における Intl.Segmenter 関連のエラーへの暫定対応
+// TODO: ライブラリがアップデートされ次第必ず削除すること
+if ((Intl as any).Segmenter == null)
+  (Intl as any).Segmenter = class {
+    segment (text: string) {
+      return text.split("")
+    }
+  }
+
 const app = createApp(App)
 app.use(router)
 app.use(i18n, messages)
