213a4a631973d4d63a3ee4a490a37e95e4232d1e
mimonelu <mimonelu@gmail.com>
2023-04-04 22:21:56 +0900

    intl-segmenter-polyfill.min.js の導入

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
 import "@/scss/main.scss"
+
+// Firefox における Intl.Segmenter 関連のエラーへの暫定対応
+// TODO: 折を見て削除すること
+import "@/composables/vendor/intl-segmenter-polyfill.min.js"
+
 import { createApp } from "vue"
 import App from "@/App.vue"
 import messages from "@/consts/messages.json"
 import i18n from "@/plugins/i18n"
 import router from "@/router"
-
-// Firefox における Intl.Segmenter 関連のエラーへの暫定対応
-// TODO: ライブラリがアップデートされ次第必ず削除すること
-if ((Intl as any).Segmenter == null)
-  (Intl as any).Segmenter = class {
-    segment (text: string) {
-      return text.split("")
-    }
-  }
 
 const app = createApp(App)
 app.use(router)
