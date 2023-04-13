d6a4578de48c7c81ce3aacc461e8b78012fb14d8
mimonelu <mimonelu@gmail.com>
2023-04-14 00:39:23 +0900

    一部のメンションとURLがリンクに変換されない不具合を修正

--- src/composables/atp-wrapper/utils/splitter.ts
+++ src/composables/atp-wrapper/utils/splitter.ts
@@ -19,24 +19,25 @@ const pattern: { [k: string]: string } = {
   mention: `(^|\\s)(@[\\w\\-]+\\.[\\w\\-\\.]+)`,
 }
 
-const regex: { [k: string]: RegExp } = {
-  url: new RegExp(pattern.url, "g"),
-  tag: new RegExp(pattern.tag, "g"),
-  mention: new RegExp(pattern.mention, "g"),
-  all: new RegExp(`${pattern.mention}|${pattern.tag}|${pattern.url}`, "g"),
-}
-
-export default (text: string): { [k: string]: any } =>
-  text
-    .split(regex.all)
-    .filter(data => !!data)
-    .map(data => {
-      const type = regex.mention.test(data)
-        ? "mention"
-        : regex.tag.test(data)
-        ? "tag"
-        : regex.url.test(data)
-        ? "url"
-        : "text"
-      return { type, value: data }
+export default (text: string): { [k: string]: any } => {
+  const regexAll = new RegExp(`${pattern.mention}|${pattern.tag}|${pattern.url}`, "g")
+  const result = text
+    .split(regexAll)
+    .filter(value => !!value)
+    .map(value => {
+      const regexMention = new RegExp(pattern.mention, "g")
+      if (regexMention.test(value)) {
+        return { type: "mention", value }
+      }
+      const regexTag = new RegExp(pattern.tag, "g")
+      if (regexTag.test(value)) {
+        return { type: "tag", value }
+      }
+      const regexUrl = new RegExp(pattern.url, "g")
+      if (regexUrl.test(value)) {
+        return { type: "url", value }
+      }
+      return { type: "text", value }
     })
+  return result
+}
