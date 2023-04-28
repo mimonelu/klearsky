83277cd7014c7e0d3716d429068ee99fadbf8544
mimonelu <mimonelu@gmail.com>
2023-04-28 13:51:40 +0900

    表示用 RichText に cleanNewlines: true を指定

--- src/components/HtmlText.vue
+++ src/components/HtmlText.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { computed, reactive, type ComputedRef } from "vue"
-import type { Entity, Facet, RichTextProps } from "@atproto/api"
+import type { Entity, Facet, RichTextOpts, RichTextProps } from "@atproto/api"
 import { RichText } from "@atproto/api"
 
 const props = defineProps<{
@@ -13,10 +13,11 @@ const state = reactive<{
   richText: ComputedRef<RichText>;
 }>({
   richText: computed(() => {
-    const options: RichTextProps = { text: props.text }
-    if (props.facets != null) options.facets = props.facets
-    if (props.entities != null) options.entities = props.entities
-    const richText = new RichText(options)
+    const rtProps: RichTextProps = { text: props.text }
+    if (props.facets != null) rtProps.facets = props.facets
+    if (props.entities != null) rtProps.entities = props.entities
+    const rtOptions: RichTextOpts = { cleanNewlines: true }
+    const richText = new RichText(rtProps, rtOptions)
     if (props.facets == null) richText.detectFacetsWithoutResolution()
     return richText
   }),
