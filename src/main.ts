0a5a2536f318386950be598795d9e89d26028b3a
mimonelu <mimonelu@gmail.com>
2023-05-01 18:15:39 +0900

    messages.json の分割

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,18 @@ import "@/composables/vendor/intl-segmenter-polyfill.min.js"
 
 import { createApp } from "vue"
 import App from "@/App.vue"
-import messages from "@/consts/messages.json"
 import i18n from "@/plugins/i18n"
 import router from "@/router"
 
+// Messages - Json
+import en from "@/consts/messages/en.json"
+import ja from "@/consts/messages/ja.json"
+
 const app = createApp(App)
 app.use(router)
+
+// Messages - Setup
+const messages = { en, ja }
 app.use(i18n, messages)
+
 app.mount("#app")
