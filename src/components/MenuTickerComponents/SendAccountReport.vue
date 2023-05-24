daec650ff5296486294f45a7f44eb02943664171
mimonelu <mimonelu@gmail.com>
2023-05-25 01:39:16 +0900

    Profile/PostMenuTicker の改修及び SendAccount/PostReport メニューの追加

---
+++ src/components/MenuTickerComponents/SendAccountReport.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { inject } from "vue"
+import SVGIcon from "@/components/SVGIcon.vue"
+import Util from "@/composables/util"
+
+const emit = defineEmits<{(event: string): void}>()
+
+const props = defineProps<{
+  user: TTUser
+}>()
+
+const mainState = inject("state") as MainState
+
+async function sendAccountReport () {
+  Util.blurElement()
+  emit("close")
+  // await mainState.openSendAccountReportPopup(props.user)
+}
+</script>
+
+<template>
+  <button @click.prevent.stop="sendAccountReport">
+    <SVGIcon name="alert" />
+    <span>{{ $t("sendAccountReport") }}</span>
+  </button>
+</template>
---
+++ src/components/MenuTickerComponents/SendPostReport.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { inject } from "vue"
+import SVGIcon from "@/components/SVGIcon.vue"
+import Util from "@/composables/util"
+
+const emit = defineEmits<{(event: string): void}>()
+
+const props = defineProps<{
+  post: TTPost
+}>()
+
+const mainState = inject("state") as MainState
+
+async function sendPostReport () {
+  Util.blurElement()
+  emit("close")
+  // await mainState.openSendPostReportPopup(props.post)
+}
+</script>
+
+<template>
+  <button @click.prevent.stop="sendPostReport">
+    <SVGIcon name="alert" />
+    <span>{{ $t("sendPostReport") }}</span>
+  </button>
+</template>
