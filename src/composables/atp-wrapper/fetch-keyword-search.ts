d68b27068724067341d7d847bc442e04890616a4
mimonelu <mimonelu@gmail.com>
2023-05-25 02:28:15 +0900

    キーワード検索の諸々の不具合を修正

--- src/composables/atp-wrapper/fetch-keyword-search.ts
+++ src/composables/atp-wrapper/fetch-keyword-search.ts
@@ -2,13 +2,15 @@ export default async function (
   this: TIAtpWrapper,
   query: string
 ): Promise<undefined | Array<any>> {
+  const encodedQuery = encodeURIComponent(query)
   const request = new Request(
-    `https://mimonelu.net:4649/https://search.bsky.social/search/posts?q=${query}`,
+    `https://mimonelu.net:4649/https://search.bsky.social/search/posts?q=${encodedQuery}`,
     { headers: { "user-agent": "Klearsky" } }
   )
   const response = await fetch(request)
+  console.log("[klearsky/fetchKeywordSearch]", response)
   const json: null | any = await response.json()
-  if (json == null) return
+  if (!Array.isArray(json)) return
   json.forEach(async (data: any) => {
     if (data.post == null) return
     const date = new Date()
@@ -16,12 +18,10 @@ export default async function (
     data.post.createdAt = date.toISOString()
     data.uri = `at://${data.user?.did}/${data.tid}`
   })
-
   json.sort((a: any, b: any) => {
     const aCreatedAt = new Date(a.post?.createdAt)
     const bCreatedAt = new Date(b.post?.createdAt)
     return aCreatedAt < bCreatedAt ? 1 : aCreatedAt > bCreatedAt ? -1 : 0
   })
-  console.log("[klearsky/fetchKeywordSearch]", json)
   return json
 }
