66edc15a76f1f5ee2ba2868829ee7e863d882193
mimonelu <mimonelu@gmail.com>
2023-06-18 21:23:53 +0900

    replacePost の不具合を修正

--- src/composables/util/replace-post.ts
+++ src/composables/util/replace-post.ts
@@ -1,5 +1,10 @@
 export default function replacePost (src: any, dst: any) {
   if (src == null || dst == null) return
+
+  // CID オブジェクトなど特殊なオブジェクトはスキップ
+  const type = Object.prototype.toString.call(src)
+  if (type !== "[object Array]" && type !== "[object Object]") return
+
   for (const key in src) {
     if (key === "translatedText" || key === "unmask") continue
     if (dst[key] === undefined) delete src[key]
