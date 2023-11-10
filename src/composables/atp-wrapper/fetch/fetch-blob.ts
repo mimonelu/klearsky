1ecba316640e2f0d45267358cea3201c96323107
mimonelu <mimonelu@gmail.com>
2023-11-11 00:55:11 +0900

    PDS移管に伴うアニメーション画像再生の不具合対応

--- src/composables/atp-wrapper/fetch/fetch-blob.ts
+++ src/composables/atp-wrapper/fetch/fetch-blob.ts
@@ -5,18 +5,28 @@ export default async function (
   cid: string,
   did?: string
 ): Promise<null | Uint8Array> {
+  // PDS移管に伴うアニメーション画像再生の不具合対応
+  // TODO: 暫定処置。同一PDSであれば getBlob を使用するように換装すること
+  const url = `https://bsky.social/xrpc/com.atproto.sync.getBlob?did=${did}&cid=${cid}`
+  const response: Response = await fetch(url)
+    .catch((error: any) => console.error("[klearsky/getBlob]", error))
+    .then((value: any) => value)
+  if (!response.ok) return null
+  return await response.blob() as unknown as Uint8Array
+
+  /*
   if (this.agent == null) return null
   if (this.session == null) return null
   const query: ComAtprotoSyncGetBlob.QueryParams = {
     did: did ?? (this.session.did as string),
     cid,
   }
-  const response: null | ComAtprotoSyncGetBlob.Response = await (
-    this.agent as BskyAgent
-  ).api.com.atproto.sync.getBlob(query)
-    .catch((error: any) => console.error("[klearsky/getBlob]", error))
-    .then((value: any) => value)
+  const response: null | ComAtprotoSyncGetBlob.Response =
+    await (this.agent as BskyAgent).api.com.atproto.sync.getBlob(query)
+      .catch((error: any) => console.error("[klearsky/getBlob]", error))
+      .then((value: any) => value)
   console.log("[klearsky/getBlob]", response)
   if (!response?.success) return null
   return response.data
+  */
 }
