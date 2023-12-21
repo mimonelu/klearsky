f4a049e06fd8256abe751cfd808ed91925d62e0f
mimonelu <mimonelu@gmail.com>
2023-12-21 19:33:12 +0900

    マイフィード保存時にリストが削除される不具合を修正

--- src/composables/atp-wrapper/fetch/fetch-feed-generators.ts
+++ src/composables/atp-wrapper/fetch/fetch-feed-generators.ts
@@ -20,6 +20,31 @@ export default async function (
     const currentGenerator = (response.data.feeds as Array<TTFeedGenerator>)
       .find((generator: TTFeedGenerator) => generator.uri === uri)
     if (currentGenerator != null) generators.push(currentGenerator)
+
+    // TODO: カスタムフィードのURIではない場合（リストフィードなど）は空の TTFeedGenerator を作成
+    //       リスト対応後に再実装すること
+    else generators.push({
+      avatar: "",
+      cid: "",
+      creator: {
+        avatar: undefined,
+        description: undefined,
+        did: "",
+        displayName: "",
+        handle: "",
+        labels: undefined,
+        viewer: {
+          muted: false,
+        },
+      },
+      description: "",
+      did: "",
+      displayName: "(Unknown feed)",
+      indexedAt: new Date().toISOString(),
+      likeCount: 0,
+      uri,
+      viewer: {},
+    })
   })
 
   return generators
