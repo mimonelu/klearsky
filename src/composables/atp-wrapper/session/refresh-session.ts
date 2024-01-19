8ddf23ad606a9301fa321910d6f96aaebe94acf1
mimonelu <mimonelu@gmail.com>
2024-01-19 12:26:21 +0900

    refreshSession のロジックを旧来のロジックに差し替え

--- src/composables/atp-wrapper/session/refresh-session.ts
+++ src/composables/atp-wrapper/session/refresh-session.ts
@@ -1,7 +1,34 @@
-import type { BskyAgent, ComAtprotoServerRefreshSession } from "@atproto/api"
+// import type { BskyAgent, ComAtprotoServerRefreshSession } from "@atproto/api"
 import Util from "@/composables/util"
 
 export default async function (this: TIAtpWrapper): Promise<undefined | Error> {
+  // TODO: 本来は @atproto/api の `com.atproto.server.refreshSession` を使用するべきだが、
+  //       不明なエラーが発生するため直接サーバを叩いている。原因がわかり次第差し替えること
+  const session = this.data.sessions[this.data.did]
+  if (session == null) return Error("noSessionError")
+  const url = `https://${session.__serviceName}/xrpc/com.atproto.server.refreshSession`
+  const request: RequestInit = {
+    method: "POST",
+    headers: { "Authorization": `Bearer ${session.refreshJwt}` },
+  }
+  const response: Response = await fetch(url, request)
+    .then((response: Response) => response)
+    .catch((error: any) => error)
+  console.log("[klearsky/refreshSession]", response)
+  if (response instanceof Error) return Error("refreshSessionError")
+  if (!response.ok) return Error("refreshSessionError")
+  const json: Error | any = await response.json()
+    .then((response: any) => response)
+    .catch((error: any) => error)
+  if (json instanceof Error) return Error("refreshSessionError")
+  if (json?.did == null) return Error("refreshSessionError")
+  this.data.did = json.did
+  this.resetSession(json)
+
+  // TODO:
+  Util.saveStorage("atp", this.data)
+
+  /*
   if (this.agent == null) return Error("noAgentError")
   if (this.session == null) return Error("noSessionError")
   const response: Error | ComAtprotoServerRefreshSession.Response =
@@ -23,4 +50,5 @@ export default async function (this: TIAtpWrapper): Promise<undefined | Error> {
 
   // TODO:
   Util.saveStorage("atp", this.data)
+  */
 }
