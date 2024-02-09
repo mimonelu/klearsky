28e2e3944f9a82dd4a200627bc8ff51a339d6f7b
mimonelu <mimonelu@gmail.com>
2024-02-09 12:26:54 +0900

    getPosts の取得限度数対策

--- src/composables/atp-wrapper/fetch/fetch-posts.ts
+++ src/composables/atp-wrapper/fetch/fetch-posts.ts
@@ -1,5 +1,6 @@
 import type { AppBskyFeedGetPosts, BskyAgent } from "@atproto/api"
 import AtpUtil from "@/composables/atp-wrapper/atp-util"
+import CONSTS from "@/consts/consts.json"
 
 export default async function (
   this: TIAtpWrapper,
@@ -11,16 +12,40 @@ export default async function (
   // 空配列はエラーとなるため却下
   if (uris.length === 0) return
 
-  const query: AppBskyFeedGetPosts.QueryParams = { uris }
-  const response: AppBskyFeedGetPosts.Response =
-    await (this.agent as BskyAgent).getPosts(query)
-      .then((value: AppBskyFeedGetPosts.Response) => value)
-      .catch((error: any) => error)
-  console.log("[klearsky/getPosts]", response)
-  if (!response.success) return false
+  const results: Array<TTPost> = []
 
-  // TODO:
-  AtpUtil.coherentResponses(response.data.posts)
+  // 取得限度数対策
+  // SEE: https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/feed/getPosts.json#L15
+  const tasks = chunkArray(uris, CONSTS.LIMIT_OF_FETCH_POSTS)
+    .map(async (uris: string[]) => {
+      const query: AppBskyFeedGetPosts.QueryParams = { uris }
+      return (this.agent as BskyAgent).getPosts(query)
+        .then((value: AppBskyFeedGetPosts.Response) => value)
+        .catch((error: any) => error)
+    })
+  const responses = await Promise.allSettled(tasks)
+  responses.forEach((response: PromiseSettledResult<any>) => {
+    console.log("[klearsky/getPosts]", response)
+    if (response == null ||
+        response.status === "rejected" ||
+        response.value instanceof Error ||
+        !response.value.success) {
+      return
+    }
 
-  return response.data.posts as Array<TTPost>
+    // TODO:
+    AtpUtil.coherentResponses(response.value.data.posts)
+
+    results.push(...(response.value.data.posts as Array<TTPost>))
+  })
+
+  return results
+}
+
+function chunkArray (array: any[], chunkSize: number): any[] {
+  const chunks = []
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize))
+  }
+  return chunks
 }
