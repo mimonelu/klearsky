35df446d774bd7592809be978fbf6f97de708dc4
mimonelu <mimonelu@gmail.com>
2024-04-21 16:59:05 +0900

    PostFeatureWrapper の実装

--- src/components/menu-items/PostFeaturesWrapper.vue
+++ src/components/menu-items/PostFeaturesWrapper.vue
@@ -1,18 +1,17 @@
 <script lang="ts" setup>
-import { reactive } from "vue"
-import MenuTicker from "@/components/menu-tickers/MenuTicker.vue"
+import { nextTick, reactive, ref } from "vue"
 import MenuTickerOpenTimeFeedsPopup from "@/components/menu-items/OpenTimeFeedsPopup.vue"
 import MenuTickerSendMention from "@/components/menu-items/SendMention.vue"
 import MenuTickerSendPostAfter from "@/components/menu-items/SendPostAfter.vue"
 import MenuTickerShowLikeUsers from "@/components/menu-items/ShowLikeUsers.vue"
 import MenuTickerShowRepostUsers from "@/components/menu-items/ShowRepostUsers.vue"
+import Popover from "@/components/popovers/Popover.vue"
 import SVGIcon from "@/components/common/SVGIcon.vue"
 
 const emit = defineEmits<{(event: string): void}>()
 
 defineProps<{
   post: TTPost
-  container?: HTMLElement
 }>()
 
 const state = reactive<{
@@ -21,56 +20,84 @@ const state = reactive<{
   display: false,
 })
 
-function showSubMenuTicker () {
-  setTimeout(() => { state.display = true }, 1)
+const trigger = ref(null)
+
+const popover = ref(null)
+
+async function open () {
+  state.display = true
+  await nextTick()
+  if (trigger.value == null || popover.value == null) {
+    return
+  }
+  ;(popover.value as typeof Popover).open(
+    trigger.value,
+    {
+      positionX: "left",
+      positionY: "middle",
+      directionX: "left",
+      directionY: "middle",
+      collideX: true,
+      collideY: true,
+      hornDirection: "right",
+      isChild: true,
+    }
+  )
+}
+
+function close () {
+  state.display = false
 }
 </script>
 
 <template>
   <button
     class="menu-ticker__sub-trigger"
+    ref="trigger"
     @click.prevent.stop
-    @mouseenter="showSubMenuTicker"
-    @mouseleave="state.display = false"
+    @mouseenter="open"
+    @mouseleave="close"
   >
     <SVGIcon name="cursorLeft" />
     <span>{{ $t("postFeatures") }}</span>
 
     <!-- ポスト機能メニュー -->
-    <MenuTicker
-      :display="state.display"
-      :container="container"
-      class="menu-ticker__sub"
+    <Popover
+      v-if="state.display"
+      ref="popover"
+      @close="close"
     >
-      <!-- メンションを送る -->
-      <MenuTickerSendMention
-        :mentionTo="post.author.handle"
-        @close="emit('close')"
-      />
+      <menu class="list-menu">
+        <!-- メンションを送る -->
+        <MenuTickerSendMention
+          :mentionTo="post.author.handle"
+          @close="emit('close')"
+        />
 
-      <!-- リポストユーザーリストポップアップトリガー -->
-      <MenuTickerShowRepostUsers
-        :uri="post.uri"
-        @close="emit('close')"
-      />
+        <!-- リポストユーザーリストポップアップトリガー -->
+        <MenuTickerShowRepostUsers
+          :uri="post.uri"
+          @close="emit('close')"
+        />
 
-      <!-- ライクユーザーリストポップアップトリガー -->
-      <MenuTickerShowLikeUsers
-        :uri="post.uri"
-        @close="emit('close')"
-      />
+        <!-- ライクユーザーリストポップアップトリガー -->
+        <MenuTickerShowLikeUsers
+          :uri="post.uri"
+          @close="emit('close')"
+        />
 
-      <!-- タイムフィードで見る -->
-      <MenuTickerOpenTimeFeedsPopup
-        :post="post"
-        @close="emit('close')"
-      />
+        <!-- タイムフィードで見る -->
+        <MenuTickerOpenTimeFeedsPopup
+          :post="post"
+          @close="emit('close')"
+        />
 
-      <!-- このポストの直後に投稿する -->
-      <MenuTickerSendPostAfter
-        :createdAt="post.record.createdAt"
-        @close="emit('close')"
-      />
-    </MenuTicker>
+        <!-- このポストの直後に投稿する -->
+        <MenuTickerSendPostAfter
+          :createdAt="post.record.createdAt"
+          @close="emit('close')"
+        />
+      </menu>
+    </Popover>
   </button>
 </template>
