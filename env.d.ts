a2883a803c34833034d095aca6a508871e14f641
mimonelu <mimonelu@gmail.com>
2024-04-27 21:52:13 +0900

    テンプレート構文内で  がTSエラーになる不具合を修正

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 import type { ComponentCustomProperties } from "vue";
 
-declare module "vue" {
+declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $t: Function;
   }
