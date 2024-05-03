9bc1cf09f33f81e056218c333d03e9b2ae2cf697
mimonelu <mimonelu@gmail.com>
2024-05-03 20:41:49 +0900

    ビルド時の HTML Minify 処理を実装

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+
+// Minify HTML
+import { createHtmlPlugin } from "vite-plugin-html"
+
 // Rollup Visualizer
 import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    // Minify HTML
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
