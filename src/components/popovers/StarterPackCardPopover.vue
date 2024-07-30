67bee1f8a47b38adae236c630d14981f66b62d1c
mimonelu <mimonelu@gmail.com>
2024-07-30 23:21:51 +0900

    スターターパックカードポップオーバーにリストページへの導線を追加

--- src/components/popovers/StarterPackCardPopover.vue
+++ src/components/popovers/StarterPackCardPopover.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { computed, inject, onMounted, reactive, ref, type ComputedRef } from "vue"
+import { useRouter } from "vue-router"
 import MenuTickerCopyTextWrapper from "@/components/menus/CopyTextWrapper.vue"
 import MenuTickerModerateWrapper from "@/components/menus/ModerateWrapper.vue"
 import MenuTickerOpenAppWrapper from "@/components/menus/OpenAppWrapper.vue"
@@ -9,6 +10,8 @@ import MenuTickerTranslateText from "@/components/menus/TranslateText.vue"
 import Popover from "@/components/popovers/Popover.vue"
 import SVGIcon from "@/components/images/SVGIcon.vue"
 import Util from "@/composables/util"
+
+const router = useRouter()
 
 const emit = defineEmits<{(event: string): void}>()
 
@@ -36,6 +39,21 @@ const state = reactive<{
     return props.starterPack?.creator.did === mainState.atp.session?.did
   }),
 })
+
+async function moveToList (type: "feeds" | "users") {
+  Util.blurElement()
+  emit("close")
+  if (props.starterPack == null) {
+    return
+  }
+  await router.push({
+    name: `list-${type}-home`,
+    query: {
+      list: props.starterPack.list?.uri ?? props.starterPack.record.list,
+      displayName: props.starterPack.record.name,
+    }
+  })
+}
 
 async function deleteStarterPack () {
   Util.blurElement()
@@ -88,6 +106,20 @@ function close () {
       v-if="starterPack != null"
       class="list-menu"
     >
+      <!-- リストフィードを見る -->
+      <button @click.prevent.stop="moveToList('feeds')">
+        <SVGIcon name="post" />
+        <span>{{ $t("showStarterPackListFeeds") }}</span>
+      </button>
+
+      <!-- リストユーザーを見る -->
+      <button @click.prevent.stop="moveToList('users')">
+        <SVGIcon name="people" />
+        <span>{{ $t("showStarterPackListUsers") }}</span>
+      </button>
+
+      <hr />
+
       <!-- リンクカードで投稿する -->
       <MenuTickerSendLinkCard
         :uri="starterPack.uri"
