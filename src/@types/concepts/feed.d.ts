53c094c781a5d8aa56c26064cff9cf235470630f
mimonelu <mimonelu@gmail.com>
2025-10-20 18:10:34 +0900

    フィードインタラクションの型定義

--- src/@types/concepts/feed.d.ts
+++ src/@types/concepts/feed.d.ts
@@ -7,6 +7,8 @@ type TTFeed = {
     [k: string]: unknown
   }
   reason?: TTReason
+  feedContext?: string
+  reqId?: string
   __cursor?: string // Injected
   __id: string // Injected
   __folding?: boolean // Injected
@@ -16,6 +18,7 @@ type TTFeed = {
 }
 
 type TTFeedGenerator = {
+  acceptsInteractions?: boolean
   avatar: string
   cid: string
   creator: TTUser
@@ -30,4 +33,23 @@ type TTFeedGenerator = {
     like?: string
     [k: string]: any
   }
+}
+
+type TTFeedInteraction = {
+  item?: string
+  event?:
+    "app.bsky.feed.defs#requestLess" |
+    "app.bsky.feed.defs#requestMore" |
+    "app.bsky.feed.defs#clickthroughItem" |
+    "app.bsky.feed.defs#clickthroughAuthor" |
+    "app.bsky.feed.defs#clickthroughReposter" |
+    "app.bsky.feed.defs#clickthroughEmbed" |
+    "app.bsky.feed.defs#interactionSeen" |
+    "app.bsky.feed.defs#interactionLike" |
+    "app.bsky.feed.defs#interactionRepost" |
+    "app.bsky.feed.defs#interactionReply" |
+    "app.bsky.feed.defs#interactionQuote" |
+    "app.bsky.feed.defs#interactionShare"
+  feedContext?: string
+  reqId?: string
 }
